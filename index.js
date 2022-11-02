513fd5dd7d469de0e097669af7335af7342223ee
Postelnicu Marius-Gil <98190287+mariusgil@users.noreply.github.com>
2022-11-02 13:43:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let button = document.getElementById("genpass")
+let passwordLength = 15
+let passwordOneEl = document.getElementById("passwordOne-el")
+let passwordTwoEl = document.getElementById("passwordTwo-el")
+
+function getRandomCharacter() {
+    let randomCharacter = Math.floor(Math.random() * characters.length)
+    return characters[randomCharacter]
+}
+
+function generateRandomPasswordOne() {
+    let randomPasswordOne = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPasswordOne+= getRandomCharacter()
+    }
+    return passwordOneEl.textContent = randomPasswordOne
+} 
+
+function generateRandomPasswordTwo() {
+    let randomPasswordTwo = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPasswordTwo+= getRandomCharacter()
+    }
+    return passwordTwoEl.textContent = randomPasswordTwo
+} 
+
+button.addEventListener("click", generateRandomPasswordOne)
+button.addEventListener("click", generateRandomPasswordTwo)
+
